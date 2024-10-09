31c403f144a2c9904e9720d813abff05101e59a6
Flipped <2399616092@qq.com>
2024-10-09 17:47:50 +0800

    错误捕捉

---
+++ division_calculator.py
@@ -0,0 +1,4 @@
+try:
+    print(5/2)
+except ZeroDivisionError:
+    print("Error")
--- wirte_message.py
+++ wirte_message.py
@@ -2,4 +2,5 @@
 flie_name='programming.txt'
 
 with open(flie_name,'w') as file_object:
-    file_object.write('Hello world!')
+    file_object.write('Hello world!\n')
+    file_object.write('I love you.\n')# .\文件写入换行
