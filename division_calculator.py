c5ee66f8775e7c2fbfacaba22cc7501556f42317
Flipped <2399616092@qq.com>
2024-10-09 18:16:15 +0800

    错误捕捉例子

--- division_calculator.py
+++ division_calculator.py
@@ -2,3 +2,19 @@ try:
     print(5/2)
 except ZeroDivisionError:
     print("Error")
+
+print("Give me two number,and I'll divide them")
+print("Enter 'Q' to quit")
+while True:
+    first_number = input("Enter First number: ")
+    if first_number == "Q":
+        break
+    second_number = input("Enter Secnond number: ")
+    if second_number == "Q":
+        break
+    try:
+        answer = int(first_number)/int(second_number)
+    except ZeroDivisionError:
+        print("Error")
+    else:
+        print(answer)
