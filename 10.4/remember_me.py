5761a89473a351276217c754da035f0c6c79b2f3
Flipped <2399616092@qq.com>
2024-10-10 12:14:50 +0800

    重构-利用函数进行重构

--- 10.4/remember_me.py
+++ 10.4/remember_me.py
@@ -1,10 +1,45 @@
-#把用户输入的名字 保存到json文件中
-# 向用户打招呼
+# #把用户输入的名字 保存到json文件中
+# # 向用户打招呼
+# import json
+# username = input('what is your name ?')
+#
+# filename_json='username.json'
+#
+# with open(filename_json,'w') as f :
+#     json.dump(username,f)
+#     print(f'{username} has been saved in',filename_json)
+
+
+### 重构
+
 import json
-username = input('what is your name ?')
+def get_stored_username():
+    """如果有名字就获取他"""
+    file ='usernames.json'  #指出usernames.json 赋值给file
+    try:#
+        with open (file,'r') as f:
+            username =json.load(f) #加载文件赋值给username
+    except FileNotFoundError:
+        return None
+    else:
+        return username
 
-filename_json='username.json'
 
-with open(filename_json,'w') as f :
-    json.dump(username,f)
-    print(f'{username} has been saved in',filename_json)
+def get_new_username():
+    """提示用户输入名字"""
+    username=input("Enter your new username: ") #提示用户输入名字
+    file='usernames,json'
+    with open (file,'w') as f:
+        json.dump(username,f) #把username值写到f 中   f->file
+    return username
+
+
+def greet_user():
+    """用户打招呼，并指出用户名字"""
+    username=get_new_username()
+    if username:
+        print(f'Welcome to back {username}')
+    else:
+        username=get_new_username()
+        print(f'Welcome to back {username}')
+greet_user()
