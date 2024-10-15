a18cf20cf920f754603084e8d0c9fa533ab4f5e3
Flipped <2399616092@qq.com>
2024-10-15 17:52:13 +0800

    骰子

--- plotly/die.py
+++ plotly/die.py
@@ -1,13 +1,13 @@
 from random import randint
-class Die():
 
 
-    '''表示一个骰子的类'''
-    def _int_(self,num_sides=6):
-        '''骰子的默认面'''
-        self.num_sides=num_sides
+class Die:
+    """A class representing a single die."""
 
+    def __init__(self, num_sides=6):
+        """Assume a six-sided die."""
+        self.num_sides = num_sides
 
-    def roll(self):# 方法使用roll（） 使用randint()返回1-面数之间的随机数
-        '''返回一个位于1和骰子面数之间的数值'''
-        return randint(1,self.num_sides)
+    def roll(self):
+        """"Return a random value between 1 and number of sides."""
+        return randint(1, self.num_sides)
