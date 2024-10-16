8412051b52637b677a35b86b4b96c8063761ab95
Flipped <2399616092@qq.com>
2024-10-16 15:13:05 +0800

    10.16-3

--- csv/eq_explore_date.py
+++ csv/eq_explore_date.py
@@ -1,7 +1,7 @@
 import json
 import pandas as pd
 import plotly.express as px
-filename='eq_data_1_day_m1.json'
+filename='eq_data_30_day_m1.json'
 with open(filename,'r') as f:
     all_eq_date=json.load(f)
     all_eq_dicts=all_eq_date['features']
@@ -15,13 +15,24 @@ for eq_dicts in all_eq_dicts:
     titles.append(title)
     lons.append(lon)
     lats.append(lat)
+data=pd.DataFrame(
+    data=zip(lons,lats,titles,msgs),columns=('经度','纬度','位置','震级',)
+)
+data.head()
 fig =px.scatter(
-    x=lons,
-    y=lats,
+    data,
+    x='经度',
+    y='纬度',
+    #lab
     range_x=[-200,200],
     range_y=[-90,90],
     width=800,height=800,
-    title='全球地震图'
+    title='全球地震图',
+    size='震级',
+    size_max=10,
+    color='震级',
+    color_continuous_scale='viridis',
+
 )
 fig.write_html('global_earthquake.html')
 fig.show()
